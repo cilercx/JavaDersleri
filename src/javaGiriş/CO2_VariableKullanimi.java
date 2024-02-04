e36ca0883a501aedfb25c225f33cde1663533f10
cilerrsahin@gmail.com <burakc34@gmail.com>
2024-02-04 16:44:37 +0300

    Kelime diye bir degisken olusturdum. burak degerini atadim.

--- src/javaGiriş/CO2_VariableKullanimi.java
+++ src/javaGiriş/CO2_VariableKullanimi.java
@@ -5,6 +5,7 @@ public class CO2_VariableKullanimi {
     public static void main(String[] args) {
 
         int sayi= 20;
+        String kelime="burak";
 
         System.out.println(sayi);
     }
